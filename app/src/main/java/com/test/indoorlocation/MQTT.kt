931740ed46c4178440b328e0cb74d3b3f3ecafc4
Iskander Yagfarov <IYagfarov@it.ru>
2019-11-16 16:47:33 +0300

    final

--- app/src/main/java/com/test/indoorlocation/MQTT.kt
+++ app/src/main/java/com/test/indoorlocation/MQTT.kt
@@ -1,2 +1,31 @@
 package com.test.indoorlocation
 
+import android.content.Context
+import io.reactivex.observers.DisposableObserver
+import org.eclipse.paho.client.mqttv3.IMqttClient
+import org.eclipse.paho.client.mqttv3.MqttClient
+import java.util.*
+
+import org.eclipse.paho.android.service.MqttAndroidClient
+
+
+//val mqttClient = MqttClient("tcp://broker.hivemq.com:1883", UUID.randomUUID().toString())
+//var clientId = MqttClient.generateClientId()
+
+fun subscribeToPoint() {
+    RxPublisher.subscribeToPoint(
+        object : DisposableObserver<DoubleArray>() {
+            override fun onComplete() {
+
+            }
+
+            override fun onNext(point: DoubleArray) {
+
+            }
+
+            override fun onError(e: Throwable) {
+
+            }
+        }
+    )
+}
--- app/src/main/java/com/test/indoorlocation/MainActivity.kt
+++ app/src/main/java/com/test/indoorlocation/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.test.indoorlocation
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.test.indoorlocation.foreground_service.ForegroundBleService
 import io.reactivex.observers.DisposableObserver
+import org.eclipse.paho.android.service.MqttAndroidClient
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var xText: TextView
     private lateinit var yText: TextView
+    //    private lateinit var  client:MqttAndroidClient
+    private lateinit var client: MqttAndroidClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         askForLocationPermissions(this)
         startForegroundService(Intent(this, ForegroundBleService::class.java))
-
-
+        client = MqttAndroidClient(
+            applicationContext, "tcp://broker.hivemq.com:1883",
+            "1313"
+        )
+        connectClient()
         xText = findViewById(R.id.x_coord)
         yText = findViewById(R.id.y_coord)
 
@@ -27,14 +39,43 @@ class MainActivity : AppCompatActivity() {
 
             }
 
+            @SuppressLint("SetTextI18n")
             override fun onNext(point: DoubleArray) {
-                xText.text = point[0].toString()
-                yText.text = point[1].toString()
+                xText.text = "X = " + point[0].toString()
+                yText.text = "Y = " + point[1].toString()
             }
 
             override fun onError(e: Throwable) {
 
             }
         })
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        client.close()
+    }
+
+    private fun connectClient() {
+//        try {
+//            val token = client.connect()
+//            token.actionCallback = object : IMqttActionListener {
+//                override fun onSuccess(asyncActionToken: IMqttToken) {
+//                    // We are connected
+//                   // Log.d(FragmentActivity.TAG, "onSuccess")
+//                }
+//
+//                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+//                    // Something went wrong e.g. connection timeout or firewall problems
+//                  //  Log.d(FragmentActivity.TAG, "onFailure")
+//
+//                }
+//            }
+//        } catch (e: MqttException) {
+//            e.printStackTrace()
+//        }
+
+
     }
 }
